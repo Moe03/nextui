5b907545388614905ceb87d6674fc5d55eceeb01
Moe Ayman <mlotov2002@gmail.com>
2025-10-27 22:10:48 +0300

    Create merge-classes.ts

---
+++ packages/core/theme/src/utils/merge-classes.ts
@@ -0,0 +1,34 @@
+import {clsx} from "@nextui-org/shared-utils";
+
+export type SlotsToClasses<S extends string> = Partial<Record<S, string | string[]>>;
+
+/**
+ * Merge classes utility function
+ * 
+ * @param itemClasses - The item classes
+ * @param itemPropsClasses - The item props classes
+ * @returns The merged classes
+ */
+export const mergeClasses = <
+  T extends SlotsToClasses<string>,
+  P extends SlotsToClasses<string>,
+>(
+  itemClasses?: T,
+  itemPropsClasses?: P,
+): T => {
+  if (!itemClasses && !itemPropsClasses) return {} as T;
+
+  const keys = new Set([
+    ...Object.keys(itemClasses || {}),
+    ...Object.keys(itemPropsClasses || {}),
+  ]);
+
+  return Array.from(keys).reduce(
+    (acc, key) => ({
+      ...acc,
+      [key]: clsx(itemClasses?.[key], itemPropsClasses?.[key]),
+    }),
+    {} as T,
+  );
+};
+
